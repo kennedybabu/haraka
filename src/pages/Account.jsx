75f6c1385d981136e6805cbf1996f826da7b6d79
kennedybabu <kennedynjogu8@gmail.com>
2022-11-10 14:02:34 +0300

    feature: add send money functionality

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -7,15 +7,23 @@ const Account = (props) => {
   const {user} = UserAuth()
   const [accBalance, setAccBalance] = useState(0) 
   const [error , setError] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
 
   const [uploadFunds, setUploadFunds] = useState(0)
+  const [sendingAmount, setSendingAmount] = useState(0)
   const [receiverEmail, setReceiverEmail] = useState('')  
 
   const [sendingMoney, setSendingMoney] = useState(false)
   const [toppingUp, setToppingUp] = useState(false)
 
-  function sendMoney(accBalance, sendAmount, receiverEmail) {
-
+  function sendMoney() {
+      if(sendingAmount > accBalance) {
+          setError('You have insufficient funds, please top up.')
+      } else if(receiverEmail && sendingAmount) {
+          let total = parseInt(accBalance ) - parseInt(sendingAmount)
+          setAccBalance(total)
+          localStorage.setItem('accBalance', total)
+      }
   }
 
   useEffect(() => {
@@ -32,6 +40,7 @@ const Account = (props) => {
         let total = parseInt(uploadFunds) + parseInt(accBalance)
         setAccBalance(total)
         localStorage.setItem('accBalance', total)
+        setSuccessMessage(`Transfer of ${sendingAmount} to ${receiverEmail} successful`)
       } else {
         setError('Please input valid number')
       }
@@ -72,16 +81,19 @@ const Account = (props) => {
       {/* transfer funds  */}
       {sendingMoney ? (
       <div className='border rounded-md my-2 p-2 md:w-[60%] md:mx-auto'>
+          {/* alert messages */}
+          {error ? <p className='bg-red-200 p-2'>{error}</p> : null}
+          {successMessage ? <p className='bg-green-200 p-2'>{successMessage}</p> : null}
          <h2 className='py-2'>Send Money</h2>
         <div className='flex flex-col'>
              <label htmlFor="email" className='font-[300]'>send to</label>
-             <input className='p-2 my-1' type="email" name='email' placeholder='email address' />
+             <input onChange={(e) => setReceiverEmail(e.target.value)} className='p-2 my-1' type="email" name='email' placeholder='email address' />
         </div>
          <div className='flex flex-col'>
              <label htmlFor="amount" className='font-[300]'>send amount</label>
-             <input className='p-2 my-1 bottom-border' type="number" placeholder='e.g 1,000' />
+             <input onChange={(e) => setSendingAmount(e.target.value)} className='p-2 my-1 bottom-border' type="number" placeholder='e.g 1,000' />
          </div>
-         <button>Transfer</button>
+         <button onClick={sendMoney}>Transfer</button>
       </div>
       ) : null }
 
